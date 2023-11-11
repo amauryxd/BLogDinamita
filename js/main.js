c65a95cb3ff5fc89fa64dc6f0ffda8b3bf8c6534
Jhonny8909 <cdmx2508@amerike.edu.mx>
2023-11-11 15:20:57 -0600

    pruebas de db

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 import {app} from "./modulos/appconfig.js";
+import { guardar } from "./modulos/guardar.js";
+import { configuracion } from "./modulos/configuracion.js";
+
+guardar();
+configuracion();
