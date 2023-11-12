29bc1d941b19af435e9f94895b4d90335e66502b
Hector Otero <cdmx2292@amerike.edu.mx>
2023-11-11 18:34:41 -0600

    Registro que aun no sirve

--- js/main.js
+++ js/main.js
@@ -2,7 +2,12 @@ import {app} from "./modulos/appconfig.js";
 import { guardar } from "./modulos/guardar.js";
 import { configuracion } from "./modulos/configuracion.js";
 import { firebaseStorage } from "./modulos/storage.js";
+import { autemail } from "./modulos/aut-email.js";
+import { autgoogle } from "./modulos/aut-google.js";
+import './modulos/registrarse.js';
 
 guardar();
 configuracion();
 firebaseStorage(app);
+autemail();
+autgoogle();
--- js/modulos/appconfig.js
+++ js/modulos/appconfig.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js"
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
@@ -15,3 +15,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const auth = getAuth (app);
