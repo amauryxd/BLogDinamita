9e8aec4b26b17713a6ae077215bfc455069b5f6c
Jonathan Pacheco <cdmx2508@amerike.edu.mx>
2023-11-13 18:12:37 -0600

    conexion a db

--- js/modulos/configuracion.js
+++ js/modulos/configuracion.js
@@ -1,3 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
 export function configuracion(){
-    
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://support.google.com/firebase/answer/7015592
+const firebaseConfig = {
+    apiKey: "AIzaSyCxTnMYqgDT3lQmPqBOHMtrr7Aav_BYDHY",
+  authDomain: "blogdinamita-ec4b5.firebaseapp.com",
+  projectId: "blogdinamita-ec4b5",
+  storageBucket: "blogdinamita-ec4b5.appspot.com",
+  messagingSenderId: "548689881854",
+  appId: "1:548689881854:web:3ea690e44843c50d8893af"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = firebase.firestore();
+
 }
--- js/modulos/guardar.js
+++ js/modulos/guardar.js
@@ -1,3 +1,12 @@
 export function guardar(){
-    
+    db.collection("Usuarios").add({
+        Nombre: document.getElementById("Nombre").value,
+        Contraseña: document.getElementById("Contraseña").value,
+    })
+    .then((docRef) => {
+        alert("registro exitoso")
+    })
+    .catch((error) => {
+        alert("error en el registro")
+    });
 }
