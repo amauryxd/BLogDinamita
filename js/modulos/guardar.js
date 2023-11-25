2bfa6fc9ef52c7a8f265981749dd3658b975575a
Jonathan Pacheco <cdmx2508@amerike.edu.mx>
2023-11-25 16:26:51 -0600

    Funcion de guardar datos terminada

--- js/modulos/guardar.js
+++ js/modulos/guardar.js
@@ -1,17 +1,24 @@
-import { getDatabase , ref, set , push} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js"
+import { getDatabase , ref, push} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js"
 
 export function guardar(){
 
     const Datos = document.querySelector("#Datos");
 
-    const txt = document.getElementById('ArtNombre');
+    const titulo = document.getElementById('ArtNombre');
+    const intro = document.getElementById('ArtIntro');
+    const principal = document.getElementById('ArtCont');
+    const conclusion = document.getElementById('ArtConcl');
+
     const db = getDatabase();
 
 
     Datos.addEventListener('submit', (e) => {
         e.preventDefault();
         push(ref(db, 'Publicacion/'),{
-            Titulo: txt.value
+            Titulo: titulo.value,
+            Introduccion: intro.value,
+            Principal: principal.value,
+            Conclusion: conclusion.value
           });
     })
 }
